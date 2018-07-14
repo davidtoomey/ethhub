b26fdc5ea2a72003b33f70b467753120ab4f1282
David Toomey <davidtoomey@Davids-MBP.fios-router.home>
2018-07-14 18:18:14 -0400

    added mongoose connection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const next = require('next');
+const mongoose = require('mongoose');
 const QuestionsController = require('./db/controllers/questionsController');
 
 const dev = process.env.NODE_ENV !== 'production';
@@ -11,6 +12,12 @@ const server = express();
 server.use(bodyParser.json());
 
 app.prepare().then(() => {
+  mongoose.connect('mongodb://localhost:27017/qanda_dev', { useNewUrlParser: true });
+  mongoose.connection
+    .once('open', () => console.log('database connection commenced'))
+    .on('error', (error) => {
+      console.warn('Error', error);
+    });
 
   server.get('/api', QuestionsController.greeting);
   server.post('/api/questions/new', QuestionsController.create);
