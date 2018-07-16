d0a8e7d3b73f9088b35c29c5e1145acc3aa1e9d7
David Toomey <yemootevad@gmail.com>
2018-07-16 12:34:06 -0400

    removed unnecessary Question interface import from top of file

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import factory from '../ethereum/factory';
-import Question from '../ethereum/question';
 import { Card, Button } from 'semantic-ui-react';
 import Layout from '../components/Layout';
 import QuestionData from '../components/QuestionData';
