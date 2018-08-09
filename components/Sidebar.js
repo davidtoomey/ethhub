19fb64e9c953efecc9f3f21b48c17326f5e743c9
David Toomey <yemootevad@gmail.com>
2018-08-09 17:09:15 -0400

    added create question and how it works link

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import { Link } from '../routes';
 
 const sidebar = () => {
   return (
     <div style={sidebarStyle}>
-      <h3>QandA</h3>
-      <a></a>
+      <h2 style={{ paddingBottom: '10px' }}>QandA</h2>
+      <div style={spacingStyle}>
+        <h3>
+          <Link route={'/questions/new'} prefetch>
+            <a style={linkStyle}>Create Question</a>
+          </Link>
+        </h3>
+      </div>
+      <div style={spacingStyle}>
+        <h3>
+          <Link route={'/'} prefetch>
+            <a style={linkStyle}>How it Works</a>
+          </Link>
+        </h3>
+      </div>
     </div>
   );
 }
@@ -19,6 +33,14 @@ const sidebarStyle = {
   left: '0',
   overflowX: 'hidden',
   padding: '10px 50px'
+}
+
+const linkStyle = {
+  color: 'white'
+}
+
+const spacingStyle = {
+  paddingBottom: '10px'
 }
 
 export default sidebar;
